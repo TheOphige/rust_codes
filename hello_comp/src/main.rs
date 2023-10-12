4e8fc37d072c3084d3232d076e13508a5a7548fe
Bing <igetheophilus02@gmail.com>
2023-10-12 07:27:22 +0100

    added some inline functions

--- hello_comp/src/main.rs
+++ hello_comp/src/main.rs
@@ -1,8 +1,5 @@
 #![deny(clippy::all)]
 
-fn main() {
-    println!("Hello world!!!");
-}
 
 // create settings.json to help you automatically format your
 // code on save
@@ -13,3 +10,35 @@ fn main() {
 
 // cargo run -q
 // cargo run --bin gugu
+
+
+
+// fn main() {
+//     println!("Hello world!!!");
+// }
+
+// function
+// fn say_hello_world(to_person: String) -> String {
+//     format!("Hello, {}!", to_person)
+// }
+// fn main() {
+//     let hello = say_hello_world(String::from("John"));
+//     println!("{}", hello);
+// }
+
+// inline function
+// fn main() {
+//     let say_hello_to 
+//         = |name: &str| format!("Hello, {}!", name);
+
+//     println!("{}", say_hello_to("World"));
+// }
+
+// full name inline function
+fn main() {
+    let full_name 
+        = |first_name: &str, last_name: &str|
+            format!("{} {}!", first_name, last_name);
+
+    println!("{}", full_name("taiye", "kehinde"));
+}
