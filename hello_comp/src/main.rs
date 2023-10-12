470d04d549d00731883770c58f303999e7e7b55c
Bing <igetheophilus02@gmail.com>
2023-10-12 07:09:16 +0100

    start hello component

---
+++ hello_comp/src/main.rs
@@ -0,0 +1,15 @@
+#![deny(clippy::all)]
+
+fn main() {
+    println!("Hello world!!!");
+}
+
+// create settings.json to help you automatically format your
+// code on save
+
+// rustup component add clippy
+// cargo install cargo-watch
+// cargo-watch -qc -x run -x clippy
+
+// cargo run -q
+// cargo run --bin gugu
