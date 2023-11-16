6e92bc0ce46f4ecb11dc6707404438af1b5c7013
Bing <igetheophilus02@gmail.com>
2023-11-16 03:44:24 +0100

    lifetimes shows your life time

---
+++ lifetimes/src/aggregator.rs
@@ -0,0 +1,81 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+pub struct Thread {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub rethread: bool,
+}
+
+// using default summary
+impl Summary for Thread {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// This parameter accepts any type that implements the specified trait. e.g. Tweets, NewsArticle
+// In the body of notify, we can call any methods on item that come from the Summary trait, such as summarize
+pub fn notify(item: &(impl Summary)) {
+    println!("Breaking news! {}", item.summarize());
+}
+// The above (impl Trait) is a simple version of the (trait bound) below
+// pub fn notify<T: Summary>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// pub fn notify(item: &(impl Summary + Display)) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// pub fn notify<T: Summary + Display>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// or using where
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+//     unimplemented!()
+// }
--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -60,75 +60,215 @@
 
 // traits
 
-use std::fmt;
+// use std::fmt;
 
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u8
+// struct Person {
+//     first_name: String,
+//     last_name: String,
+//     age: u8,
+// }
+
+// // a trait for println! while using the display syntax {}
+// impl fmt::Display for Person {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(
+//             f, "{} {} is {} years old", 
+//             self.first_name, self.last_name, self.age
+//         )
+//     }
+// }
+
+// trait HasFullName {
+//     fn full_name(&self) -> String;
+// }
+
+
+// impl HasFullName for Person {
+//     fn full_name(&self) -> String {
+//         format!("{} {}", self.first_name, self.last_name)
+//     }
+// }
+
+// trait CanInitializeWithFullName {
+//     fn new(full_name: &str) -> Self;
+// }
+
+// impl CanInitializeWithFullName for Person {
+//     fn new(full_name: &str) -> Self {
+//         let parts: Vec<&str> = full_name.split(' ').collect();
+//         Person {
+//             first_name: parts[0].to_string(),
+//             last_name: parts[1].to_string(),
+//             age: 30,
+//         }
+//     }
+// }
+
+// fn print_full_name_and_age(value: &impl HasFullName) {
+//     println!("{}", value.full_name())
+// }
+
+// fn print_details<T>(value: &T) where
+//     T: HasFullName + CanRun,
+// {
+//     println!("{}", value.full_name())
+// }
+
+// trait CanRun {
+//     fn run(&self);
+// }
+
+// impl CanRun for Person {
+//     fn run(&self) {
+
+//     }
+// }
+
+// fn main() {
+//     let person = Person::new("John Doe");
+//     println!("{}", person);
+//     println!("{}", Person::full_name(&person));
+//     print_full_name_and_age(&person);
+//     print_details(&person);
+// }
+
+// Generics
+
+// the function largest is generic over some type T. 
+// This function has one parameter named list, which is 
+// a slice of values of type T. The largest function will 
+// return a reference to a value of the same type T.
+// To enable comparisons, the standard library has the 
+// std::cmp::PartialOrd trait that you can implement on 
+// types
+
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
 
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
 // a trait for println! while using the display syntax {}
-impl fmt::Display for Person {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f, "{} {} is {} years old", 
-            self.first_name, self.last_name, self.age
-        )
+// use std::fmt;
+// impl<T: fmt::Display, U: fmt::Display> fmt::Display for Point<T, U> {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(
+//             f, "x is {}, y is {}.", 
+//             self.x, self.y
+//         )
+//     }
+// }
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+    fn y(&self) -> &U {
+        &self.y
     }
 }
 
-trait HasFullName {
-    fn full_name(&self) -> String;
-}
-
-
-impl HasFullName for Person {
-    fn full_name(&self) -> String {
-        format!("{} {}", self.first_name, self.last_name)
-    }
-}
-
-trait CanInitializeWithFullName {
-    fn new(full_name: &str) -> Self;
-}
-
-impl CanInitializeWithFullName for Person {
-    fn new(full_name: &str) -> Self {
-        let parts: Vec<&str> = full_name.split(' ').collect();
-        Person {
-            first_name: parts[0].to_string(),
-            last_name: parts[1].to_string(),
-            age: 30,
+impl<T, U> Point<T, U> {
+    fn mixup<T2, U2>(self, other: Point<T2, U2>) -> Point<T, U2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
 }
 
-fn print_full_name_and_age(value: &impl HasFullName) {
-    println!("{}", value.full_name())
-}
-
-fn print_details<T>(value: &T) where
-    T: HasFullName + CanRun,
-{
-    println!("{}", value.full_name())
-}
-
-trait CanRun {
-    fn run(&self);
-}
-
-impl CanRun for Person {
-    fn run(&self) {
-
+//concrete type f32, meaning 
+// we don’t declare any types after impl.
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
 
+// importing traits & types from aggregator
+mod aggregator;
+use aggregator::{Summary, Tweet, NewsArticle, Thread, notify};
+
 
 fn main() {
-    let person = Person::new("John Doe");
-    println!("{}", person);
-    println!("{}", Person::full_name(&person));
-    print_full_name_and_age(&person);
-    print_details(&person);
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    //println!("{}", both_float);
+    println!("x is {:?}, y is {:?}.", both_float.x, both_float.y);
+
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {} & p.y = {}", p.x(), p.y());
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    let newsarticle = NewsArticle {
+        headline: String::from("Nigeria is the best country in the world"),
+        location: String::from("oyo"),
+        author: String::from("jamiu_olushola"),
+        content: String::from(
+            "definitely people in that country are technologically inclined",
+        ),
+    };
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    let thread = Thread {
+        username: String::from("juju_boy"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        rethread: false,
+    };
+    
+    println!("1 new tweet: {}", tweet.summarize());
+    println!("1 new thread: {}", thread.summarize());
+    println!("1 news article: {}", newsarticle.summarize());
+    println!("New article available! {}", article.summarize());
+    notify(&newsarticle)
 }
+
