a270593cdd8b1861ba6cbd4a591b9cfc9fd7f65b
Bing <igetheophilus02@gmail.com>
2023-12-20 14:30:05 +0100

    lifetimes is not individual lifetimes but lifetime of one related to another

---
+++ learn_async/src/main.rs
@@ -0,0 +1,64 @@
+// CARGO TOML [FUTURES, tokio] ON CRATES.IO
+
+#![deny(clippy::all)]
+
+// use futures::executor::block_on;
+
+// async fn get_name() -> String {
+//     "John".to_string()
+// }
+
+// fn main() {
+//     let name = block_on(get_name());
+//     println!("Hello, {}!", name);
+// }
+
+// use tokio::time{sleep, Duration};
+
+// async fn call_api_one() -> String {
+//     sleep(Duration::from_secs(1)).await;
+//     "One".to_string();
+// }
+
+// async fn call_api_two() -> String {
+//     sleep(Duration::from_secs(1)).await;
+//     "Two".to_string();
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let one = call_api_one().await;
+//     println!("{}", one);
+//     let two = call_api_two().await;
+//     println!("{}", two);
+// }
+
+
+
+use tokio::time::{sleep, Duration};
+use futures::Future;
+
+fn call_api_one() -> impl Future<Output = String> {
+    async {
+        sleep(Duration::from_secs(1)).await;
+        "One".to_string();
+    }
+    
+}
+
+fn call_api_two() -> impl Future<Output = String> {
+    async {
+        sleep(Duration::from_secs(1)).await;
+        "Two".to_string();
+    }
+    
+}
+
+
+#[tokio::main]
+async fn main() {
+    let one = call_api_one().await;
+    println!("{}", one);
+    let two = call_api_two().await;
+    println!("{}", two);
+}
--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,6 +1,50 @@
 // lifetimes Traits Generics
 #![deny(clippy::all)]
 
+
+// use std::fmt::Display;
+
+// fn longest_with_an_announcement<'a, T>(
+//     x: &'a str,
+//     y: &'a str,
+//     ann: T,
+// ) -> &'a str
+// where
+//     T: Display,
+// {
+//     println!("Announcement! {}", ann);
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let string1 = String::from("abcd");
+//     let string2 = "xyz";
+
+//     let result = longest_with_an_announcement(
+//         string1.as_str(),
+//         string2,
+//         "Today is someone's birthday!",
+//     );
+//     println!("The longest string is {}", result);
+// }
+
+// struct ImportantExcerpt<'a> {
+//     part: &'a str,
+// }
+
+// fn main() {
+//     let novel = String::from("Call me Ishmael. Some years ago...");
+//     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+//     let i = ImportantExcerpt {
+//         part: first_sentence,
+//     };
+// }
+
+
 // fn get_full_name() -> &'static str {
 //     "John Doe"
 // }
