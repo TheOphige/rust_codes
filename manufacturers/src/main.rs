0649c1d037714ad0ae272e2dc2b3f125d874adc0
Bing <igetheophilus02@gmail.com>
2024-01-04 16:09:49 +0100

    add dependencies

--- manufacturers/src/main.rs
+++ manufacturers/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
+#![deny(clippy::all)]
+
+use std::env;
+
+const API_URL: &str = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json";
+
+struct Manufacturer<'a> {
+    name: Option<&'a str>,
+    common_name: Option<&'a str>,
+    country: Option<&'a str>,
+}
+
+trait Contains {
+    fn contains(&self, needle: &str) -> bool;
+}
+
+impl<'a> Contains for Manufacturer<'a> {
+    fn contains(&self, needle: &str) -> bool {
+        self.name.unwrap_or_default().contains(needle) 
+        || self.common_name.unwrap_or_default().contains(needle)
+        || self.country.unwrap_or_default().contains(needle)
+    }
+}
+
+impl<'a> Manufacturer<'a> {
+    fn description(&self) -> String {
+        let name = self.name.unwrap_or_default();
+        let common_name = self.common_name.unwrap_or_default();
+        let country = self.country.unwrap_or_default();
+        format!("\tName: {}\n\tCommon Name: {}\n\tCountry: {}", name, common_name, country)
+    }
+} 
+
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
+
+
+// cargo-watch -qc -x 'run -- BMW' -x clippy
