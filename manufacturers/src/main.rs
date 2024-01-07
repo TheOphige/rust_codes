bd05b05218c99551f92d68cb304d887b4289ee41
Bing <igetheophilus02@gmail.com>
2024-01-07 11:37:01 +0100

    fixed manufacturers

--- manufacturers/src/main.rs
+++ manufacturers/src/main.rs
@@ -1,6 +1,6 @@
 #![deny(clippy::all)]
 
-use std::env;
+use std::{env, sync::Arc};
 
 const API_URL: &str = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json";
 
@@ -33,8 +33,57 @@ impl<'a> Manufacturer<'a> {
 
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <search term>", args[0]);
+        return Ok(());
+    }
+    let keyword = &args[1];
+
+    let client = reqwest::Client::new();
+    let res = client
+        .get(API_URL)
+        .send()
+        .await?;
+    let response = res.json::<serde_json::Value>().await?;
+
+    let manufacturer_json = response
+        .as_object()
+        .unwrap()
+        .iter()
+        .find(|(key, _)| key == &"Results")
+        .unwrap()
+        .1
+        .as_array()
+        .unwrap()
+        .iter();
+
+    let manufacturers = manufacturer_json.map(|manufacturer_json| {
+        let obj = manufacturer_json.as_object().unwrap();
+        let country = obj.get("Country").unwrap().as_str();
+        let common_name = obj.get("Mfr_CommonName").unwrap().as_str();
+        let name = obj.get("Mfr_Name").unwrap().as_str();
+        Manufacturer {
+            name,
+            common_name,
+            country,
+        }
+    });
+
+    let found_manufacturers = manufacturers.filter(|manufacturer| manufacturer.contains(keyword))
+        .collect::<Vec<_>>();
+
+    if found_manufacturers.is_empty() {
+        Err("No manufacturers found".into())
+    } else {
+        println!("Found {} manufacturers:", found_manufacturers.len());
+        for (index, man) in found_manufacturers.iter().enumerate() {
+            println!("Manufacturer #{}", index + 1);
+            println!("{}", man.description());
+        }
+        Ok(())
+    }
 }
 
 
